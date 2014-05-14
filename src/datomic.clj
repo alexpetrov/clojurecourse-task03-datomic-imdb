1d4ac3051835d4555f4b4fad4069556fe74f7442
alexpetrov <alexpetrov.rb@gmail.com>
2014-05-14 22:38:40 +0400

    Done with import data

--- src/datomic.clj
+++ src/datomic.clj
@@ -27,7 +27,7 @@
 
   {:db/ident :feature/id
    :db/valueType :db.type/string
-   :db/unique :db.unique/identity ;; I think identity here is better than value, because there is no need to have whole db unique values; This needs to use lookup ref feature
+   :db/unique :db.unique/identity ;; I think identity here is better than value, because there is no need to have whole db unique values; This needs to use lookup ref feature. Also it make entity upsertable
    :db/cardinality :db.cardinality/one
    :db/id (d/tempid :db.part/db)
    :db.install/_attribute :db.part/db}
@@ -76,6 +76,53 @@
   (alter-var-root #'conn (constantly (d/connect db-url)))
   @(d/transact conn schema))
 ;; (reset)
+
+(defn set-year [feature entity]
+  (if (contains? feature :year)
+    (assoc entity :feature/year (:year feature))
+    entity))
+
+(defn set-series [feature entity]
+  (if (contains? feature :series)
+    (assoc entity :feature/series (:series feature))
+    entity))
+
+(defn set-endyear [feature entity]
+  (if (contains? feature :endyear)
+    (assoc entity :feature/endyear (:endyear feature))
+    entity))
+
+(defn set-episode [feature entity]
+  (if (contains? feature :episode)
+    (assoc entity :feature/episode (:episode feature))
+    entity))
+
+(defn set-season [feature entity]
+  (if (contains? feature :season)
+    (assoc entity :feature/season (:season feature))
+    entity))
+
+
+(defn construct-feature [feature]
+  [(->> {:db/id (d/tempid :db.part/user)
+        :feature/type (:type feature)
+        :feature/id (:id feature)
+        :feature/title (:title feature)}
+        (set-year feature)
+        (set-series feature)
+        (set-endyear feature)
+        (set-episode feature)
+        (set-season feature)
+
+)])
+
+;; (construct-feature {:type :series, :id "\"#JustDating\" (2014)", :title "#JustDating", :year 2014})
+
+;; (construct-feature {:type :series, :id "\"30 for 30\" (2009)", :title "30 for 30", :year 2009, :endyear 2014})
+
+;; (construct-feature {:type :episode, :id "\"#JustDating\" (2014) {(#1.1)}", :series "\"#JustDating\" (2014)", :season 1, :episode 1, :year 2014})
+
+
 ;; Формат файла:
 ;; { :type  =>   :series | :episode | :movie | :video | :tv-movie | :videogame
 ;;   :id    =>   str,  unique feature id
@@ -87,32 +134,43 @@
 ;;   :episode => long, only for episode, optional. Episode number
 ;; }
 ;; hint: воспользуйтесь lookup refs чтобы ссылаться на features по внешнему :id
+;; TODO: Сделать функцию, которая будет формировать entity для feature
 (defn import []
   (with-open [rdr (io/reader "features.2014.edn")]
     (doseq [line (line-seq rdr)
             :let [feature (edn/read-string line)]]
-      (d/transact conn [{:db/id (d/tempid :db.part/user)
-                         :feature/type (:type feature)
-                         :feature/id (:id feature)
-                         :feature/title (:title feature)
-                       ;;  :feature/year (:year feature)
-                         }]))))
+      (d/transact conn (construct-feature feature)))))
+;;(reset)
 ;;(import)
 ;; Найти все пары entity указанных типов с совпадающими названиями
 ;; Например, фильм + игра с одинаковым title
 ;; Вернуть #{[id1 id2], ...}
 ;; hint: data patterns
-;; (println (d/q '[:find ?n ?id
-;;        :where
-;;        [?n :feature/id ?id]]
-;;      (d/db conn)))
-;; (d/q '[:find ?n ?id
-;;        :where
-;;        [?n :feature/id ?id]]
-;;      (d/db conn))
+;; (println (d/q '[:find (first ?n) :where [?n :feature/id "\"#JustDating\" (2014)"]] (d/db conn)))
+
+
+;; (println (d/q '[:find ?year :where [?n :feature/year ?year]] (d/db conn)))
+;; (println (d/q '[:find ?title :where [?n :feature/title ?title]] (d/db conn)))
+
+;; (println (d/q '[:find ?endyear :where [?n :feature/endyear ?endyear]] (d/db conn)))
+;; (println (d/q '[:find ?n ?series :where [?n :feature/series ?series]] (d/db conn)))
+;; (println (d/q '[:find (first ?season) :where [?n :feature/season ?season]] (d/db conn)))
+;; (println (d/q '[:find (first ?n) ?episode :where [?n :feature/episode ?episode]] (d/db conn)))
+;; (println (d/q '[:find ?n ?type ?dbident :where [?n :feature/type ?type][?type :db/ident ?dbident]] (d/db conn)))
+
+
+;; (d/q '[:find ?n ?id :where [?n :feature/id ?id]] (d/db conn))
 ;; (println "hello datomic")
+;; (siblings (db) :movie :videogame)
 (defn siblings [db type1 type2]
-  :TODO)
+  (d/q '[:find ?id1 ?id2
+         :in $ ?type1 ?type2
+         :where
+         [?id1 :feature/type ?type1]
+         [?id2 :feature/type ?type2]
+         [?id1 :feature/title ?title]
+         [?id2 :feature/title ?title]]
+       db type1 type2))
 
 ;; Найти сериал(ы) с самым ранним годом начала
 ;; Вернуть #{[id year], ...}
