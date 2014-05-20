b831cef268d127552b931fa6765cc23e787df56b
alexpetrov <alexpetrov.rb@gmail.com>
2014-05-21 00:31:00 +0400

    Done work on improvements Use consistent db value across all business query in siblings function
    siblings: removed unnecessary clause from query and added some comments
    and questions
    
    Clean up code a bit
    
    Made lookup-ref feature for episode -> feature ref
    
    Added index by title. Throw away unnecessary optimization in siblings.
    
    Done with keyword type for type attr
    
    Add profiling to tests and cleanup code

--- src/datomic.clj
+++ src/datomic.clj
@@ -12,15 +12,8 @@
 
 ;; Hint: сделайте extenal id из feature :id
 (def schema [
-  [:db/add (d/tempid :db.part/user) :db/ident :series] ;; ? or better :feature.type/series
-  [:db/add (d/tempid :db.part/user) :db/ident :episode]
-  [:db/add (d/tempid :db.part/user) :db/ident :movie]
-  [:db/add (d/tempid :db.part/user) :db/ident :video]
-  [:db/add (d/tempid :db.part/user) :db/ident :tv-movie]
-  [:db/add (d/tempid :db.part/user) :db/ident :videogame]
-
   {:db/ident :feature/type
-   :db/valueType :db.type/ref
+   :db/valueType :db.type/keyword
    :db/cardinality :db.cardinality/one
    :db/id (d/tempid :db.part/db)
    :db.install/_attribute :db.part/db}
@@ -35,6 +28,7 @@
   {:db/ident :feature/title
    :db/valueType :db.type/string
    :db/cardinality :db.cardinality/one
+   :db/index true
    :db/id (d/tempid :db.part/db)
    :db.install/_attribute :db.part/db}
 
@@ -51,7 +45,7 @@
    :db.install/_attribute :db.part/db}
 
   {:db/ident :feature/series
-   :db/valueType :db.type/string
+   :db/valueType :db.type/ref
    :db/cardinality :db.cardinality/one
    :db/id (d/tempid :db.part/db)
    :db.install/_attribute :db.part/db}
@@ -76,23 +70,37 @@
   (alter-var-root #'conn (constantly (d/connect db-url)))
   @(d/transact conn schema))
 
+;; (def feature1 {:type :episode :id "id" :year 1923 :title "title" :endyear 2013 :series "series" :season "season" :episode "episode"})
+;; (:type )
+;; (construct-feature {:type :movie :id "id" :year 1923 :title "title" :endyear 2013 :series "series" :season "season" :episode "episode"})
+;; (construct-feature {:type :episode :id "id" :year 1923 :title "title" :endyear 2013 :series "series" :season "season" :episode "episode"})
+;; (construct-feature {
+;;                     :type :episode
+;;                     :id "id"
+;;                     :year 1923
+;;                     :title "title"
+;;                     :endyear 2013
+;;                     :series "series"
+;;                     :season "season"
+;;                     :episode "episode"
+;;})
+(defn- remove-empty-keys [map]
+  (into {}
+        (for [[k v] map
+              :when v] [k v])))
 
-;; FIXME: It must be better name for this, or some more idiomatic way
-(defn- cr-attr-map [key value]
-  (if value {key value}))
-;; (cr-attr-map :a nil)
-;; (merge {:a "3"} nil)
-;; (construct-feature-new {:type :movie :id "id" :year 1923 :title "title" :endyear 2013 :series "series" :season "season" :episode "episode"})
 (defn- construct-feature [feature]
-  [(merge {:db/id (d/tempid :db.part/user)}
-         (cr-attr-map :feature/type (:type feature))
-         (cr-attr-map :feature/id (:id feature))
-         (cr-attr-map :feature/title (:title feature))
-         (cr-attr-map :feature/year (:year feature))
-         (cr-attr-map :feature/endyear (:endyear feature))
-         (cr-attr-map :feature/series (:series feature))
-         (cr-attr-map :feature/season (:season feature))
-         (cr-attr-map :feature/episode (:episode feature)))])
+  (->> {:db/id (d/tempid :db.part/user)
+        :feature/type (:type feature)
+        :feature/id (:id feature)
+        :feature/title (:title feature)
+        :feature/year (:year feature)
+        :feature/endyear (:endyear feature)
+        :feature/season (:season feature)
+        :feature/episode (:episode feature)
+        :feature/series (when-let [series (:series feature)]
+                          [:feature/id series])}
+       remove-empty-keys))
 
 ;; Формат файла:
 ;; { :type  =>   :series | :episode | :movie | :video | :tv-movie | :videogame
@@ -105,68 +113,60 @@
 ;;   :episode => long, only for episode, optional. Episode number
 ;; }
 ;; hint: воспользуйтесь lookup refs чтобы ссылаться на features по внешнему :id
-;; TODO каким образом нужно воспользоваться lookup refs не очень понятно.
-;; Это либо очевидно и подругому сделать нельзя, или я что-то не понимаю.
+
+;; TODO: I don't understand how this can work.
+;; It only can work if all episodes goes after series in data file.
+;; Otherwize there will be no series for episode when latter gonna be inserted
+(defn import-data []
+  (with-open [rdr (io/reader "features.2014.edn")]
+    (doseq [line (line-seq rdr)
+            :let [feature (edn/read-string line)]]
+      @(d/transact conn [(construct-feature feature)]))))
+
+;;(def import import-data)
+;; In this realization we insert episodes after series
 (defn import []
-  (let [count-imported-features (atom 0)]
+  (let [episodes (atom [])]
     (with-open [rdr (io/reader "features.2014.edn")]
       (doseq [line (line-seq rdr)
-            :let [feature (edn/read-string line)]]
-        (d/transact conn (construct-feature feature))
-         (swap! count-imported-features inc)))
-    (print "Imported " @count-imported-features " features")))
+            :let [feature (edn/read-string line)]
+            :let [edn-feature (construct-feature feature)]]
+        (if (= :episode (:feature/type edn-feature))
+          (swap! episodes conj edn-feature)
+          (d/transact conn [edn-feature]))
+        ))
+    (d/transact conn @episodes)))
 ;;(time (reset))
 ;;(time (import))
+;; import with lookup ref tooks 68195 msecs
+;; import without lookupt ref tooks 139109 msecs (x2)
+;; with index on title ti tooks 65421.360474 msecs. The same as without index
+
+;; import
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Найти все пары entity указанных типов с совпадающими названиями
 ;; Например, фильм + игра с одинаковым title
 ;; Вернуть #{[id1 id2], ...}
 ;; hint: data patterns
-;; (d/q '[:find (count ?t)
+;; (time (d/q '[:find (count ?t)
 ;;        :where
 ;;        [?f1 :feature/title ?t]
 ;;        [?f2 :feature/title ?t]
 ;;        [(not= ?f1 ?f2)]]
-;;  (db)) ;; => [[1405]]
-(defn count-features-by-type [type]
-  (ffirst (d/q '[:find (count ?f1)
-       :in $ ?type
-       :where
-       [?f1 :feature/type ?type]]
-               (db) type)))
-
-(defn order-types-by-count [type1 type2]
-  (let [type1-count (count-features-by-type type1)
-        type2-count (count-features-by-type type2)]
-    (if (> type1-count type2-count) [type2 type1 true]
-        [type1 type2 false])))
-;; (order-types-by-count :movie :videogame) ;; => [:videogame :movie :true]
-;; (order-types-by-count :videogame :movie) ;; => [:videogame :movie :false]
-
-;; (count-features-by-type :movie) ;; => 20929
-;; (count-features-by-type :videogame) ;; => 153
-;; (siblings (db) :videogame :movie)
-;; (time (siblings (db) :movie :videogame))
-;; (time (siblings (db) :videogame :movie))
-;;(swap-pairs-in-set #{[1 2] [2 4]}) ;; =>
-(defn swap-pairs-in-set [set]
-    (into #{} (map (fn [[x y]] [y x]) set)))
+;;  (db))) ;;"Elapsed time: 1503.547588 msecs"
+;; => [[1405]]
 
 (defn siblings [db type1 type2]
-  (let [[t1 t2 swapped?] (order-types-by-count type1 type2)
-        result (d/q '[:find ?id1 ?id2
+  (d/q '[:find ?id1 ?id2
          :in $ ?type1 ?type2
          :where
-         [?f1 :feature/type ?type1]
          [?f1 :feature/title ?t]
          [?f2 :feature/title ?t]
-         [(not= ?f1 ?f2)]
-
+         [?f1 :feature/type ?type1]
          [?f2 :feature/type ?type2]
          [?f1 :feature/id ?id1]
          [?f2 :feature/id ?id2]]
-       db t1 t2)]
-    (if (true? swapped?) (swap-pairs-in-set result)
-           result)))
+       db type1 type2))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; oldest-series
 
@@ -209,8 +209,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; longest-season
-(defn get-episode-by-id [db episode-id]
-  (d/q '[:find ?]))
 
 (defn eid [db feature-id]
   (->> (d/q '[:find ?id
@@ -236,11 +234,9 @@
          :where
          [?f :feature/season ?season]
          [?f :feature/series ?series-id]
-;;         [?f :feature/id ?id]
          [?f :feature/episode ?episode]]
        db series-id)
-      (sort-by second >))
-  )
+      (sort-by second >)))
 
 (defn count-episodes-by-season-in-series [db series-id]
   (d/q '[:find ?season (count ?episode) ?series-id
@@ -248,7 +244,6 @@
          :where
          [?f :feature/season ?season]
          [?f :feature/series ?series-id]
-;;         [?f :feature/id ?id]
          [?f :feature/episode ?episode]]
        db series-id))
 ;; (time (get-episodes-by-season-in-series (db)  "\"Let's Ask America\" (2012)"))
@@ -262,6 +257,9 @@
          [?f :feature/id]]
        db))
 ;; (time (print (count-all-features (db))));;=>43125 а строк в файле 48957
+;; After switch to keyword:
+;; (time (print (count-all-features (db))));;=>26765 а строк в файле 48957
+;;
 
 (defn third [coll]
   (nth coll 2))
@@ -270,18 +268,20 @@
   (d/q '[:find ?series-id ?season (count ?f)
          :where
          [?f :feature/season ?season]
-         [?f :feature/series ?series-id]]
+         [?f :feature/series ?s]
+         [?s :feature/id ?series-id]]
        db))
 ;; (print (count-episodes-by-season (db)))
+;; (time (count-episodes-by-season (db)))
 ;; Найти 3 сериала с наибольшим количеством серий в сезоне
 ;; Вернуть [[id season series-count], ...]
 ;; hint: aggregates, grouping
 
 (defn longest-season [db]
   (->> (count-episodes-by-season db)
-      (sort-by third >)
-      (take 3)
-      vec))
+       (sort-by third >)
+       (take 3)
+       vec))
 ;; (time (longest-season (db)))
 
 
@@ -290,11 +290,12 @@
          :where
          [?f :feature/title ?title]
          [?f :feature/type ?type]
-         [?episode :db/ident :episode]
-;;         []
-         [(not= ?type ?episode)]
-         ]
+;;         [?episode :db/ident :episode]
+;;                  [(not= ?type ?episode)]
+         [(not= ?type :episode)]
+]
        db))
+;; (time (count-titles (db)))
 ;; Найти 5 самых популярных названий (:title). Названия эпизодов не учитываются
 ;; Вернуть [[count title], ...]
 ;; hint: aggregation, grouping, predicates
@@ -305,4 +306,4 @@
       (take 5)
       vec))
 
-;;(time (popular-titles (db)))
+;;(time (popular-titles (db))) ;; without index on title it tooks 605.346835 msecs
--- test/test_datomic.clj
+++ test/test_datomic.clj
@@ -4,29 +4,35 @@
     clojure.test))
 
 (deftest test-siblings
-  (is (= (siblings (db) :movie :videogame)
+  (println "test siblings :movie :videogame")
+  (time (is (= (siblings (db) :movie :videogame)
          #{["Destiny (2014/I)"    "Destiny (2014) (VG)"]
            ["Destiny (2014/II)"   "Destiny (2014) (VG)"]
            ["Destiny (2014/III)"  "Destiny (2014) (VG)"]
            ["The Amazing Spider-Man 2 (2014)" "The Amazing Spider-Man 2 (2014) (VG)"]
-           ["Clandestine (2014)"  "Clandestine (2014) (VG)"]}))
-  (is (= (siblings (db) :series :videogame)
+           ["Clandestine (2014)"  "Clandestine (2014) (VG)"]})))
+  (println "test siblings :series :videogame")
+  (time (is (= (siblings (db) :series :videogame)
          #{["\"Destiny\" (2013)"          "Destiny (2014) (VG)"]
            ["\"Game of Thrones\" (2011)"  "Game of Thrones (2014) (VG)"]
-           ["\"Sonic Boom\" (2014)"       "Sonic Boom (2014) (VG)"]})))
+           ["\"Sonic Boom\" (2014)"       "Sonic Boom (2014) (VG)"]}))))
 
 (deftest test-oldest-series
-  (is (= (oldest-series (db))
-         #{["\"Today\" (1952)" 1952]})))
+  (println "test-oldest-series")
+  (time (is (= (oldest-series (db))
+         #{["\"Today\" (1952)" 1952]}))))
 
 (deftest test-longest-season
-  (is (= (longest-season (db))
+  (println "test-longest-season")
+  (time (is (= (longest-season (db))
          [["\"Familie\" (1991)"           1 88]
           ["\"Let's Ask America\" (2012)" 2 87]
-          ["\"Belmonte\" (2013)"          1 78]])))
+          ["\"Belmonte\" (2013)"          1 78]]))))
+
 
 (deftest test-popular-titles
-  (is (= (popular-titles (db))
+  (println "test-popular-titles")
+  (time (is (= (popular-titles (db))
          [[9 "Alone"]
           [8 "Home"]
           [6 "Broken"]
@@ -38,4 +44,4 @@
          ;;  [6 "Destiny"]
          ;;  [6 "Broken"]]
 
-)))
+))))
